cffffd64d23d2b92e531de180887a9af8c14af0d
Animesh Singh <87444276+Animesh0764@users.noreply.github.com>
2026-01-20 23:42:29 +0530

    Update API_BASE_URL to use localhost for local development and adjust Vercel configuration for environment variable

--- client/constants/config.ts
+++ client/constants/config.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = 
-  process.env.EXPO_PUBLIC_API_URL || 'https://assignment-matiks.onrender.com';
+  process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8080';
 
 export const API_ENDPOINTS = {
   leaderboard: '/api/leaderboard',
